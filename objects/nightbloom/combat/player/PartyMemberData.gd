b3e551725a3d48acdf552cd8838a809f71f585d6
Nick Marino <nicholas.marino@zero.nyc>
2026-01-30 12:44:28 -0500

    Add character portraits to party member resources

--- objects/nightbloom/combat/player/PartyMemberData.gd
+++ objects/nightbloom/combat/player/PartyMemberData.gd
@@ -3,6 +3,7 @@ class_name PartyMemberData
 
 @export var display_name: StringName
 @export var character_scene: PackedScene
+@export var character_portrait: Texture2D
 @export var anim_locomotion: String
 @export var anim_receive_hit: String
 @export var attacks: Array[AttackData]
